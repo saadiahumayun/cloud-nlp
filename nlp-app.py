5c3c20b624e6b86ba915d189197b13407dfcce2e
saadiahumayun <34272512+saadiahumayun@users.noreply.github.com>
2023-09-26 09:26:33 +0500

    Update nlp-app.py

--- nlp-app.py
+++ nlp-app.py
@@ -1,4 +1,11 @@
-import streamlit as st
 import openai
+import streamlit as st
+
+# Initialize OpenAI API and server base
+openai.api_key = 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
+openai.api_base = "http://zanino.millennium.berkeley.edu:8000/v1"
+
+# Set Streamlit layout
+st.set_page_config(layout="wide")
 
 st.title('Web App for Generating Terraform Scripts')
